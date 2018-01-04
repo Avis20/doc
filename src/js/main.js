4477a18b160cd72d0b232929df3054a246eca36e
Орлов Ярослав <orlov.avis@yandex.ru>
2018-01-04 23:15:51 +0300

    hello

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() {
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+});
+
+var ToC =
+"<nav role='navigation' class='table-of-contents'>" +
+    "<h2>On this page:</h2>" +
+"<ul>";
+
+var newLine, self, title, link;
+
+$("body h3").each(function() {
+
+    self = $(this);
+    title = self.text();
+    link = "#" + self.attr("id");
+
+    newLine = "<li><a href='" + link + "'>" + title + "</a></li>";
+
+    ToC += newLine;
+
+});
+
+ToC += "</ul></nav>";
+
+$(".toc").prepend(ToC);
