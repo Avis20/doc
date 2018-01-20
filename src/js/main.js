09da7b3dbef8138218c943fccfa1b802ad72113d
Орлов Ярослав <orlov.avis@yandex.ru>
2018-01-20 16:25:38 +0300

    commit from script

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
 });
 
 var ToC =
@@ -27,3 +27,22 @@ $("#main_content h2").each(function() {
 ToC += "</ul></nav>";
 
 $(".toc").prepend(ToC);
+
+var here = location.href.split('/').slice(3);
+
+newLine = '<li><a href="../">Home</a></li>';
+
+for( var i = 0; i < here.length; i++ ) {
+    var part = here[i];
+    var text = part.toUpperCase();
+
+    var link = '/' + here.slice( 0, i + 1 ).join('/');
+
+    if ( /HTML$/.test(text) ){
+        text = text.split('.')[0];
+    }
+
+    newLine += "<li><a href='" + link + "'>" + text + "</a></li>";
+}
+
+$(".breadcrumbs").prepend(newLine);
