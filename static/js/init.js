1cbfea90d5adeb7f90bd07d3181e8cfc1f135d61
Орлов Ярослав <orlov.avis@yandex.ru>
2018-02-03 19:04:32 +0300

    hello

---
+++ static/js/init.js
@@ -0,0 +1,39 @@
+
+$(function() {
+    $("#wrapper").load("/doc/source/templates/header.html", function() {
+        var template    = Handlebars.compile( $("#entry-template").html() ),
+            content     = $("#content").html(),
+            date        = $("#date").html(),
+            html        = template({ title: "Hello", author: "Orlov Yaroslav", date: date, content: content });
+        $("#wrapper").css("background-color", "");
+        $("#wrapper").css("position", "");
+        $("#content").html(html);
+    });
+
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+
+    var ToC =
+    "<nav role='navigation' class='table-of-contents'>" +
+    "<ul class=\"multi\">";
+
+    var newLine, self, title, link;
+
+    $(".site-main h3").each(function() {
+
+        self = $(this);
+        title = self.text();
+        link = "#" + self.attr("id");
+
+        newLine = "<li><a href='" + link + "'>" + title + "</a></li>";
+
+        ToC += newLine;
+
+    });
+
+    ToC += "</ul></nav>";
+
+    $(".toc").prepend(ToC);
+
+});
